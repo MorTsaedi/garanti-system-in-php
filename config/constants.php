45737e22a2f624bfc8e25cc2b532bc810e1d80a8
morteza saedi <109029529+MorTsaedi@users.noreply.github.com>
2024-08-05 12:07:43 +0330

    data deleted

--- config/constants.php
+++ config/constants.php
@@ -4,16 +4,17 @@ if (!isset($_SESSION)){
 	session_start();
 }
 	
-    define('SITEURL', 'https://digi2040.ir/garanti/');
+    define('SITEURL', 'localhost/guaranti/');
+    //main admin username:
     define('ADMIN','admin');
     define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'digiikal_imei');
-    define('DB_PASSWORD', '5214@Digi');
-    define('DB_NAME', 'digiikal_imei');
-    define('SMS_UNAME', 'digi2040');
-    define('SMS_PASS', '5214@Digi');
-    define('SMS_FROM', '3000505');
-    define('SMS_JSPD', 'https://ippanel.com/services.jspd');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'guaranti');
+    define('SMS_UNAME', '');
+    define('SMS_PASS', '');
+    define('SMS_FROM', '');
+    define('SMS_JSPD', '');
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
 ?>
